05ec24b55778f324ab63885f9f97bc68fe682fbd
Goodbadboy <selfmanademola@gmail.com>
2025-07-26 21:08:01 +0100

    added the entry file

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import ngrok from 'ngrok';
+import bot from './controllers/lib/bot.js';
+import { getAllPendingLimitOrders } from './controllers/lib/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, async (err) => {
+    if (err) {
+        return;
+    }
+    try {
+        const url = await ngrok.connect({
+            addr: PORT,
+        });
+        if (bot && bot.telegram) {
+            await bot.telegram.setWebhook(`${url}/`);
+        } else {
+            console.error("⚠️ bot or bot.telegram is undefined");
+        }
+    } catch (e) {
+        console.error("🔥 Error starting ngrok or setting webhook:");
+    }
+});
+
+setInterval(() => {
+    getAllPendingLimitOrders()
+}, 20 * 1000);
